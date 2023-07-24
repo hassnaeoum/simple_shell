6ba951009eae463163953f3d3657b3b8a7aa2122
lili852 <majedabremli@gmail.com>
2023-07-24 15:52:10 -0700

    second updates

--- shell.h
+++ shell.h
@@ -16,7 +16,24 @@ int _atoi(char *ntr);
 int env_func(void);
 int exit_func(char **tokens, char *buffer);
 int handle_builtin(char **tokens, char *buffer);
+void free_av(char **av);
+void _puts(char **str);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+int _setenv(const char *name, const char *value, int overwrite);
+int _unsetenv(char *name);
+int check_spaces_around_command(char *input);
+int check_spaces_tabs(char *str);
+int excute_cmd(char **av, char *buff);
+char *_getenv(char *name);
+int _strlen(char *str);
+char *_strcat(char *dest, char *src);
+char *_get_cmp(char *name);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strncpy(char *dest, char *src, int n);
+void tokenize(char *buffer, char **tokens);
 int _putchar(char c);
 
-
-#endif
+#endiif
---
+++ strfunctions1.c
@@ -0,0 +1,60 @@
+#include "shell.h"
+
+/**
+ * _strncpy - function that copies a string
+ *
+ * @dest: destination string
+ * @n: integer that specify the max num of char to be copied.
+ * @src: source srting
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: pointer of the first string to be compared.
+ * @s2: pointer of the second string to be compared.
+ * Return: integer.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+/**
+ * _strncmp - compares first n bytes of s1 and s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes to compare
+ * Return: int
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i = 0;
+
+	while (s1[i] && (i < n))
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (0);
+}
