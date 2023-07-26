34e3b021f63e758d3b24edb05b09a4d4880599b4
hassnaeoum <oumezdii@gmail.com>
2023-07-26 02:31:05 -0700

    group task

--- functions.c
+++ functions.c
@@ -1,6 +1,10 @@
 #include "shell.h"
 
-/* Free memory allocated to tokens */
+/**
+* free_tokens - free memory allocated to tokens
+*
+* @tokens: array of tokens
+*/
 void free_tokens(char **tokens)
 {
 int i = 0;
@@ -12,13 +16,18 @@ i++;
 }
 }
 
-/* Execute commands */
+/**
+* execute_command - executes commands
+*
+* @tokens: array of tokens
+* @buffer: input buffer
+* Return: exit_status
+*/
 int execute_command(char **tokens, char *buffer)
 {
 pid_t pid;
 char *cmd_path = NULL;
 int exit_status = handle_builtin(tokens, buffer);
-
 if (exit_status == -1)
 {
 cmd_path = find_command_path(tokens[0]);
@@ -27,14 +36,12 @@ if (!cmd_path)
 _puts("Command not found"); /* Custom error message */
 return (2);
 }
-
 pid = fork();
 if (pid == -1)
 {
 perror("fork");
 exit(EXIT_FAILURE);
 }
-
 if (pid == 0)
 {
 if (execve(cmd_path, tokens, environ) == -1)
@@ -51,15 +58,16 @@ waitpid(pid, &exit_status, 0);
 if (exit_status != 0)
 exit_status = 2;
 }
-
 if (_strcmp(cmd_path, tokens[0]) != 0)
 free(cmd_path);
 }
-
-return exit_status;
+return (exit_status);
 }
-
-/* Get the value of an environment variable */
+/**
+* get_environment_variable - get the value of an environment variable
+* @name: name of the environment variable
+* Return: value of the environment variable or NULL if not found
+*/
 char *get_environment_variable(char *name)
 {
 int i = 0;
@@ -71,10 +79,14 @@ if (strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=')
 return (&environ[i][name_len + 1]);
 }
 
-return NULL;
+return (NULL);
 }
 
-/* Search for the command in the directories listed in PATH */
+/**
+* find_command_path - search for the command in the directories listed in PATH
+* @command: command to find
+* Return: pointer to the command's path or NULL if not found
+*/
 char *find_command_path(char *command)
 {
 char *path, *path_copy, *path_token, *command_path;
@@ -87,7 +99,6 @@ if (path)
 path_copy = _strdup(path);
 command_length = _strlen(command);
 path_token = strtok(path_copy, ":");
-
 while (path_token)
 {
 directory_length = _strlen(path_token);
@@ -96,11 +107,10 @@ _strcpy(command_path, path_token);
 _strcat(command_path, "/");
 _strcat(command_path, command);
 _strcat(command_path, "\0");
-
 if (stat(command_path, &buffer) == 0)
 {
 free(path_copy);
-return command_path;
+return (command_path);
 }
 else
 {
@@ -108,21 +118,21 @@ free(command_path);
 path_token = strtok(NULL, ":");
 }
 }
-
 free(path_copy);
-
-if (stat(command, &buffer) == 0) /* If the command is found in the current directory */
+if (stat(command, &buffer) == 0) /*If the comd is found in the current dir*/
 {
-return command;
+return (command);
+}
+return (NULL);
+}
+return (NULL);
 }
 
-return NULL;
-}
-
-return NULL;
-}
-
-/* Tokenize the input buffer */
+/**
+* tokenize_input - tokenize the input buffer
+* @buffer: input buffer
+* @tokens: array to store tokens
+*/
 void tokenize_input(char *buffer, char **tokens)
 {
 size_t token_len;
@@ -130,17 +140,14 @@ const char *delim = " \t\n";
 char *token = NULL;
 int count = 0;
 char *buffer_copy = NULL;
-
 buffer_copy = _strdup(buffer);
 token = strtok(buffer_copy, delim);
-
 for (count = 0; token; count++)
 {
 token_len = _strlen(token);
-tokens[count] = malloc(sizeof(char *) * token_len);
+tokens[count] = malloc(sizeof(char *) * (token_len + 1));
 _strncpy(tokens[count], token, token_len + 1);
 token = strtok(NULL, delim);
 }
-
 free(buffer_copy);
 }
